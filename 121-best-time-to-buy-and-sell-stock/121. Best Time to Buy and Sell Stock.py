609a8f30ce841093dd66fd22ccabc3419b690556
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2023-10-01 20:17:46 -0700

    Runtime: 832 ms (74.03%), Memory: 27.2 MB (95.35%) - Gitcode

--- 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
+++ 121-best-time-to-buy-and-sell-stock/121. Best Time to Buy and Sell Stock.py
@@ -1,22 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxp = 0
-
+        res = 0
         l = 0
-        r = 1
 
-        while r < len(prices):
+        for r in range(len(prices)):
             if prices[l] < prices[r]:
-                curr = prices[r] - prices[l]
-                maxp = max(maxp, curr)
+                res = max(res, prices[r] - prices[l])
             else:
                 l = r
-            r += 1
-
-        return maxp
-
         
-                
-        
-        
+        return res
         
