bfa9ee14957e65c4d22d2d51ac87dadc33b10e23
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-01-28 18:40:05 -0800

    Runtime: 299 ms (36.42%), Memory: 28 MB (10.77%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -6,20 +6,29 @@ class Solution:
         ROWS = len(grid)
         COLS = len(grid[0])
 
+        visit = set()
         res = 0
+
        
         def bfs(r,c):
-            grid[r][c] = 0
+            visit.add((r,c))
 
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                if r+dr in range(ROWS) and c+dc in range (COLS) and grid[r+dr][c+dc] == "1":
-                    bfs(r + dr, c + dc)
+            if r+1 in range(ROWS) and c in range (COLS) and grid[r+1][c] == "1" and (r+1,c) not in visit:
+                bfs(r+1,c) 
+
+            if r in range(ROWS) and c+1 in range (COLS) and grid[r][c+1] == "1" and (r,c+1) not in visit:
+                bfs(r,c+1)
+
+            if r-1 in range(ROWS) and c in range (COLS) and grid[r-1][c] == "1" and (r-1,c) not in visit:
+                bfs(r-1,c)
+
+            if r in range(ROWS) and c-1 in range (COLS) and grid[r][c-1] == "1" and (r,c-1) not in visit:
+                bfs(r,c-1)
                 
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1":
+                if grid[r][c] == "1" and (r,c) not in visit:
                     bfs(r,c)
                     res += 1
         return res
