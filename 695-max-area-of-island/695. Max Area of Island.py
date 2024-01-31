8d29447c2b75a414a9b65805abf3dca9ca1a888c
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-01-30 18:25:24 -0800

    Runtime: 113 ms (67.48%), Memory: 17.4 MB (81.04%) - Gitcode

--- 695-max-area-of-island/695. Max Area of Island.py
+++ 695-max-area-of-island/695. Max Area of Island.py
@@ -1,32 +1,25 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        
         rows = len(grid)
         cols = len(grid[0])
-
-        currmax = 0
         res = 0
 
         def dfs(r,c):
 
             if r not in range(rows) or c not in range(cols) or grid[r][c] == 0:
                 return 0
-            nonlocal currmax
+
             grid[r][c] = 0
-            print(currmax, r,c )
-            currmax += 1
 
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            currmax = 1
+            currmax += dfs(r + 1,c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
 
-            return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+            return currmax
 
-        
         for r in range(rows):
-            for c in range(cols):             
-                res = max(res, dfs(r,c))
-                currmax = 0
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    res = max(res, dfs(r,c))
 
         return res
             
