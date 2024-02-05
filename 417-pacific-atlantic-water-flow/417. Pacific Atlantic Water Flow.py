f4a11ab3add40fb6f523d613e962a774717677fb
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-02-04 18:35:17 -0800

    Runtime: 197 ms (97.11%), Memory: 18.2 MB (74.41%) - Gitcode

--- 417-pacific-atlantic-water-flow/417. Pacific Atlantic Water Flow.py
+++ 417-pacific-atlantic-water-flow/417. Pacific Atlantic Water Flow.py
@@ -24,10 +24,17 @@ class Solution:
             dfs(r, 0, pac, heights[r][0])
             dfs(r, cols-1, atl, heights[r][cols-1])
 
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) in pac and (r,c) in atl:
-                    res.append([r,c])
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if (r,c) in pac and (r,c) in atl:
+        #             res.append([r,c])
+        
+        # for x in pac:
+        #     if x in atl:
+        #         res.append(x)
+        
+        res = [x for x in pac if x in atl]
+
         return res
 
                 
