a88bbb6620abaf506fe43960bed2ae4f99d9741b
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-02-20 13:01:39 -0800

    Runtime: 371 ms (69.05%), Memory: 18.7 MB (70.48%) - Gitcode

---
+++ 743-network-delay-time/743. Network Delay Time.py
@@ -0,0 +1,26 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adj = defaultdict(list)
+
+        for u,v,w in times:
+            adj[u].append((v,w)) #dest, weight
+
+        minheap = [[0, k]] #0 cost, k starting
+        visit = set()
+        t = 0
+
+        while minheap:
+            if len(visit) == n:
+                return t
+            w, v = heapq.heappop(minheap)
+            if v in visit:
+                continue
+            visit.add(v)
+            t = max(t, w) # max of all ?
+
+            for n2, w2 in adj[v]:
+                heapq.heappush(minheap, (w + w2, n2))
+            
+        if n == len(visit):
+            return t
+        return -1
