cda281790294ce7ccb90a38636f4f977223664f3
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-02-28 19:06:00 -0800

    Runtime: 56 ms (54.66%), Memory: 16.7 MB (59.47%) - Gitcode

--- 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
+++ 746-min-cost-climbing-stairs/746. Min Cost Climbing Stairs.py
@@ -1,15 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.append(0)
+        a = cost[0]
+        b = cost[1]
+        
+        for i in range(2, len(cost)):
+            temp = b
+            b = cost[i] + min(a,b)
+            a = temp
 
-        a = cost[-2]
-        b = cost[-1]
-
-        for i in range(len(cost) - 3, -1, -1): #start at pos[-2]
-            temp = a
-            a = cost[i] + min(a,b)
-            b = temp
-
-
-        return min(a,b)
+        return min(a ,b)
         
