7ec03a3b1c55f2f554812c9fe930b29e48a8a48a
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-05 18:37:29 -0800

    Runtime: 797 ms (56.49%), Memory: 16.9 MB (92.98%) - Gitcode

--- 322-coin-change/322. Coin Change.py
+++ 322-coin-change/322. Coin Change.py
@@ -1,6 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float("inf")] * (amount + 1)
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
 
         for a in range(1, amount + 1):
@@ -8,7 +8,7 @@ class Solution:
                 if a-c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a - c])
         
-        if dp[amount] == float("inf"):
+        if dp[amount] == amount + 1:
             return -1
         
         return dp[amount]
