70435feb656516e96a8d847094ace9de0411adb7
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-07 15:48:16 -0800

    Runtime: 43 ms (40.40%), Memory: 16.7 MB (71.85%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -1,13 +1,31 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+        # dp = [False] * (len(s) + 1)
+        # dp[len(s)] = True
 
-        for i in range(len(s) - 1, - 1, -1):
+        # for i in range(len(s) - 1, - 1, -1):
+        #     for w in wordDict:
+        #         if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
+        #             dp[i] = dp[i + len(w)]    
+        #             print(i, i+len(w), s[i: i + len(w)])
+        #         if dp[i]:
+        #             break
+        
+        # print(dp)
+        # return dp[0]
+
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s)+1):
             for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]    
+                if (i - len(w)) >= 0 and s[i - len(w): i] == w:
+                    dp[i] = dp[i - len(w)]    
+                    print(i, i-len(w), s[i - len(w): i], dp[i])
                 if dp[i]:
                     break
 
-        return dp[0] 
+        
+        print(dp)
+
+        return dp[len(s)] 
