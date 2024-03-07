eb86ebbaa53e8db3c0ad12f155a5dcdf2c3f6552
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-07 15:54:04 -0800

    Runtime: 44 ms (34.73%), Memory: 16.6 MB (71.85%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -17,15 +17,11 @@ class Solution:
         dp = [False] * (len(s) + 1)
         dp[0] = True
 
-        for i in range(len(s)+1):
+        for i in range(1, len(s)+1):
             for w in wordDict:
                 if (i - len(w)) >= 0 and s[i - len(w): i] == w:
                     dp[i] = dp[i - len(w)]    
-                    print(i, i-len(w), s[i - len(w): i], dp[i])
                 if dp[i]:
                     break
-
-        
-        print(dp)
 
         return dp[len(s)] 
