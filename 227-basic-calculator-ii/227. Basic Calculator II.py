e1099b62751947d31aa748377f05aea3e4f4d248
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-09 19:01:24 -0800

    Runtime: 237 ms (5.02%), Memory: 19.4 MB (74.03%) - Gitcode

--- 227-basic-calculator-ii/227. Basic Calculator II.py
+++ 227-basic-calculator-ii/227. Basic Calculator II.py
@@ -3,27 +3,38 @@ class Solution:
         ops = {'+','-','*','/'}
         
         stack = []
+        lastnum = 0
 
         curop = '+'
 
         curnum = 0
+
+        res = 0
 
         for i,c in enumerate(s):
             if c.isdigit():
 
                 curnum = curnum*10 + int(c)
             if c in ops or i == len(s) - 1:
+                print(res, lastnum, curnum)
                 if curop == '+':
-                    stack.append(curnum)
+                    res += lastnum
+                    lastnum = curnum
+                    #stack.append(curnum)
                 elif curop == '-':
-                    stack.append(-curnum)
+                    res += lastnum
+                    lastnum = -curnum
+
+                    #stack.append(-curnum)
                 elif curop == '*':
-                    stack[-1] *= curnum
+                    lastnum *= curnum
+                    #stack[-1] *= curnum
                 elif curop == "/":
-                    stack[-1] =int(stack[-1] / curnum) 
+                    lastnum = int(lastnum/ curnum)
+                    #stack[-1] =int(stack[-1] / curnum) 
                 
                 curnum = 0
                 curop = c
 
-        return sum(stack)
+        return res+lastnum
                 
