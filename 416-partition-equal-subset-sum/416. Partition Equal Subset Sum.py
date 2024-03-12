7ad94527bb6a96030e76b9ccfc4dc41c86be2c0d
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-11 22:58:53 -0700

    Runtime: 346 ms (78.21%), Memory: 17.8 MB (75.59%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
@@ -12,6 +12,8 @@ class Solution:
             for t in dp:
                 if t == target:
                     return True
+                if (t+ nums[i] == target):
+                    return True
                 nextdp.add(t + nums[i])
             dp = nextdp
         return True if target in dp else False
