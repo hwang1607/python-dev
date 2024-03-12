6b89c0ce83dc9dae42178ec8f530af0876b49721
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-11 22:57:55 -0700

    Runtime: 465 ms (74.91%), Memory: 18.5 MB (60.44%) - Gitcode

--- 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
+++ 416-partition-equal-subset-sum/416. Partition Equal Subset Sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        if total % 2 != 0:
+        if sum(nums) % 2 != 0:
             return False
 
         dp = set()
