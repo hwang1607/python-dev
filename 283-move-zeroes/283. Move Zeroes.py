451999499519c9fbac3a8c347e7889f2d1e7d62e
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-22 22:29:08 -0700

    Runtime: 2506 ms (5.00%), Memory: 18.1 MB (78.68%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -3,9 +3,10 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == 0:
                 for j in range(i+1, len(nums)):
+    
 
                     if nums[j] != 0:
                         nums[i], nums[j] = nums[j], nums[i]
                         break
-                    # if j == len(nums) - 1:
-                    #     return
+                    if j == len(nums) - 1:
+                        return
