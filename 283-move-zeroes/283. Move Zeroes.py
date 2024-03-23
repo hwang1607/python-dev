30cb1b8e4267add18f563c49c74f9ad2e4443d8b
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-22 21:49:29 -0700

    Runtime: 130 ms (45.71%), Memory: 18.1 MB (78.68%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -1,10 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        left = 0
-
-        for right in range(len(nums)):
-            if nums[right] != 0:
-                nums[right], nums[left] = nums[left], nums[right]
-                left += 1
-        
-        return nums
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0 
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+= 1
+                        
