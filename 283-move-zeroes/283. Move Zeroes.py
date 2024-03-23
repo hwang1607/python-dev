215c99f26423e6900fad8513e4cecce4e1e7f720
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-22 21:50:38 -0700

    Runtime: 130 ms (45.71%), Memory: 18.1 MB (78.68%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -3,11 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i+1, len(nums)):
-                    if nums[j] != 0:
-                        nums[i], nums[j] = nums[j], nums[i]
-                        break
-                        return
+        l = 0 
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+= 1
                         
