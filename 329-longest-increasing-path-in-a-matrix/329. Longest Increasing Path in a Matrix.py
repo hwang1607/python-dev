24e93673c39209f46c46158607ecebd30f190f0e
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-27 15:58:52 -0700

    Runtime: 334 ms (41.08%), Memory: 21.9 MB (14.80%) - Gitcode

--- 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
@@ -1,23 +1,36 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        dp = {}  # (r, c) -> LIP
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        dp = {} #(r,c) : val
 
-        def dfs(r, c, prevVal):
-            if r < 0 or r == ROWS or c < 0 or c == COLS or matrix[r][c] <= prevVal:
+        def dfs(r, c, preval):
+            if r >= ROWS or c >= COLS or c < 0 or r < 0:
                 return 0
-            if (r, c) in dp:
-                return dp[(r, c)]
+            if matrix[r][c] <= preval:
+                return 0
+            if (r,c) in dp:
+                return dp[(r,c)]
+            
+            
+            currval = matrix[r][c]
 
             res = 1
-            res = max(res, 1 + dfs(r + 1, c, matrix[r][c]))
-            res = max(res, 1 + dfs(r - 1, c, matrix[r][c]))
-            res = max(res, 1 + dfs(r, c + 1, matrix[r][c]))
-            res = max(res, 1 + dfs(r, c - 1, matrix[r][c]))
-            dp[(r, c)] = res
+            res = max(res, 1 + dfs(r+1,c, currval))
+            res = max(res, 1 + dfs(r-1,c, currval))
+            res = max(res, 1 + dfs(r,c+1, currval))
+            res = max(res, 1 + dfs(r,c-1, currval))
+
+            dp[(r,c)] = res
+
             return res
+
+
 
         for r in range(ROWS):
             for c in range(COLS):
-                dfs(r, c, -1)
+                dfs(r,c, -1)
+        
         return max(dp.values())
+        
+        
