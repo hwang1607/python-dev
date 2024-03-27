bcefd8f645f2c8bacf4c98a173e33e9854c629ad
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-27 15:54:27 -0700

    Runtime: 443 ms (11.61%), Memory: 22.1 MB (13.30%) - Gitcode

---
+++ 329-longest-increasing-path-in-a-matrix/329. Longest Increasing Path in a Matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        dp = {} #(r,c) : val
+
+        def dfs(r, c, preval):
+            if r not in range(ROWS) or c not in range(COLS):
+                return 0
+            if matrix[r][c] <= preval:
+                return 0
+            if (r,c) in dp:
+                return dp[(r,c)]
+            
+            
+            currval = matrix[r][c]
+
+            res = 1
+            res = max(res, 1 + dfs(r+1,c, currval))
+            res = max(res, 1 + dfs(r-1,c, currval))
+            res = max(res, 1 + dfs(r,c+1, currval))
+            res = max(res, 1 + dfs(r,c-1, currval))
+
+            dp[(r,c)] = res
+
+            return res
+
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r,c, -1)
+        
+        return max(dp.values())
+        
+        
