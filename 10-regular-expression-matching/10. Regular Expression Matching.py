3d0ea50201a55db419567b9d6dd833396d4bcd05
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-03-31 23:31:12 -0700

    Runtime: 42 ms (68.86%), Memory: 17 MB (9.78%) - Gitcode

--- 10-regular-expression-matching/10. Regular Expression Matching.py
+++ 10-regular-expression-matching/10. Regular Expression Matching.py
@@ -8,12 +8,16 @@ class Solution:
                 return True
             if j >= len(p):
                 return False
+            if (i,j) in dp:
+                return dp[(i,j)]
             
             match = i < len(s) and (s[i] == p[j] or p[j] == ".")
             if (j+1) < len(p) and p[j+1] == '*':
-                return (dfs(i, j+2) or (match and dfs(i+1,j)))
+                dp[(i,j)] = (dfs(i, j+2) or (match and dfs(i+1,j)))
+                return dp[(i,j)]
             if match:
-                return dfs(i+1, j+1)
-            
+                dp[(i,j)] = dfs(i+1, j+1)
+                return dp[(i,j)]
+            dp[(i,j)] = False
             return False
         return dfs(0,0)
