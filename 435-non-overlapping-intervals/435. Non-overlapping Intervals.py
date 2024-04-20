bca8d8a899c841b9f4f8322786dc52028d8c5fa8
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-04-19 17:58:30 -0700

    Runtime: 1092 ms (51.36%), Memory: 55.2 MB (58.14%) - Gitcode

--- 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
+++ 435-non-overlapping-intervals/435. Non-overlapping Intervals.py
@@ -2,7 +2,6 @@ class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
 
         intervals.sort(key = lambda x : x[1])
-        print(intervals)
 
         cur = intervals[0]
         res = 0
@@ -10,8 +9,8 @@ class Solution:
         for i in range(1, len(intervals)):
             if intervals[i][0] < cur[1]:
                 res += 1
-                continue
-            cur = intervals[i]
+            else:
+                cur = intervals[i]
         
         return res
         
