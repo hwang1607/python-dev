bfde59cbdcd06d923ec7c9349ccaabd0cc2aab68
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-09 15:37:46 -0700

    Runtime: 50 ms (5.35%), Memory: 16.9 MB (6.14%) - Gitcode

--- 88-merge-sorted-array/88. Merge Sorted Array.py
+++ 88-merge-sorted-array/88. Merge Sorted Array.py
@@ -3,24 +3,32 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        end = len(nums1) - 1
+        if n == 0:
+            return
+        total = m+n-1
         m -= 1
         n -= 1
+        print(n)
+       # p = m + n -1 # last index of nums 1
 
-        while n >= 0:
-            #print(m,n,end)
-            if m >= 0 and nums1[m] > nums2[n]:
-                nums1[end] = nums1[m]
+        for i in range(total, -1, -1):
+            print(i, m, n)
+            if m == -1:
+                nums1[i] = nums2[n]
+                n-= 1
+                continue
+            if n == -1:
+                nums1[i] = nums1[m]
                 m -= 1
-        
-            else:
-                print("d")
-                nums1[end] = nums2[n]
-                n -= 1
+                continue
 
-            end -= 1
+            if nums1[m] >= nums2[n]:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n-= 1
         
-     
-                
+
 
         
