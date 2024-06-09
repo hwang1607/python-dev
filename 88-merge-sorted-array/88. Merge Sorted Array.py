51b6260e5eac4656528e059232ec4ffc70875f0f
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-09 15:16:07 -0700

    Runtime: 42 ms (40.36%), Memory: 17.3 MB (6.14%) - Gitcode

--- 88-merge-sorted-array/88. Merge Sorted Array.py
+++ 88-merge-sorted-array/88. Merge Sorted Array.py
@@ -8,18 +8,15 @@ class Solution:
         n -= 1
 
         while n >= 0:
-            print(m,n,end)
-
-            if m < 0 or nums1[m] <= nums2[n]:
-                print("d")
-                nums1[end] = nums2[n]
-                n -= 1
-
-            else:
+            #print(m,n,end)
+            if m >= 0 and nums1[m] > nums2[n]:
                 nums1[end] = nums1[m]
                 m -= 1
         
-            
+            else:
+                print("d")
+                nums1[end] = nums2[n]
+                n -= 1
 
             end -= 1
         
