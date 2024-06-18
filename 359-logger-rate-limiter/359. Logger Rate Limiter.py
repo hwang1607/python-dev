f052e7bd698810271906d5ac15fe99bdf5d1b3cf
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-17 22:01:06 -0700

    Runtime: 166 ms (5.53%), Memory: 23.1 MB (67.92%) - Gitcode

--- 359-logger-rate-limiter/359. Logger Rate Limiter.py
+++ 359-logger-rate-limiter/359. Logger Rate Limiter.py
@@ -1,24 +1,23 @@
-class Logger:
+class Logger(object):
 
     def __init__(self):
-        self.seen = {} #msg : timestamp
-        self.q = deque()
-        
+        """
+        Initialize your data structure here.
+        """
+        self._msg_dict = {}
+    
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        """
+        if message not in self._msg_dict:
+            # case 1). add the message to print
+            self._msg_dict[message] = timestamp
+            return True
 
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        while self.q and timestamp - self.q[0][1] >= 10:
-            del self.seen[self.q[0][0]]
-            self.q.popleft()
-
-        if message in self.seen:
+        if timestamp - self._msg_dict[message] >= 10:
+            # case 2). update the timestamp of the message
+            self._msg_dict[message] = timestamp
+            return True
+        else:
             return False
-        
-        self.seen[message] = timestamp
-        self.q.append((message, timestamp))
-        return True
-        
-
-
-# Your Logger object will be instantiated and called as such:
-# obj = Logger()
-# param_1 = obj.shouldPrintMessage(timestamp,message)
