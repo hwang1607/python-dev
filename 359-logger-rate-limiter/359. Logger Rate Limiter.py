d60f98fa61fdf5bf5a55a840f2ffe882abecd81e
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-17 22:02:04 -0700

    Runtime: 126 ms (18.23%), Memory: 23.2 MB (53.70%) - Gitcode

--- 359-logger-rate-limiter/359. Logger Rate Limiter.py
+++ 359-logger-rate-limiter/359. Logger Rate Limiter.py
@@ -2,19 +2,16 @@ class Logger:
 
     def __init__(self):
         self.seen = {} #msg : timestamp
-        self.q = deque()
         
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        while self.q and timestamp - self.q[0][1] >= 10:
-            del self.seen[self.q[0][0]]
-            self.q.popleft()
-
         if message in self.seen:
+            if timestamp - self.seen[message] >= 10:
+                self.seen[message] = timestamp
+                return True
             return False
         
         self.seen[message] = timestamp
-        self.q.append((message, timestamp))
         return True
         
 
