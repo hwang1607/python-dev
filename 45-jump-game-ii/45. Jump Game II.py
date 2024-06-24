a228b032ae7d1308cd3be07f9fdb464993d0b4cf
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-23 18:12:53 -0700

    Runtime: 99 ms (87.01%), Memory: 17.3 MB (97.86%) - Gitcode

--- 45-jump-game-ii/45. Jump Game II.py
+++ 45-jump-game-ii/45. Jump Game II.py
@@ -1,14 +1,27 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        farthest = 0
+        left = 0
+        right = 0
 
-        memo = [float("inf")] * len(nums)
-        memo[len(nums) - 1] = 0
+        while right < len(nums) - 1:
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+            left = right + 1
+            right = farthest
+            jumps += 1
+        
+        return jumps
 
-        for i in range(len(nums) -1, -1, -1):
-            for j in range(nums[i] + 1):
-                if i + j < len(nums) and memo[i+j] != float("inf"):
-                    memo[i] = min(memo[i], memo[i+j] + 1)
 
-        print(memo)
-        return memo[0]
-                
+        # dp = [float("inf")] * len(nums)
+        # dp[-1] = 0
+
+        # for i in range(len(nums) - 2, -1, -1):
+        #     for j in range(nums[i] + 1):
+        #         if i+j < len(nums):
+        #             dp[i] = min(dp[i], 1 + dp[i+j])
+        
+        # return dp[0]
+
