d0bff2cf0f331c8392b6732a5b939a1155373af7
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-24 23:31:16 -0700

    Runtime: 36 ms (84.05%), Memory: 16.8 MB (62.12%) - Gitcode

--- 274-h-index/274. H-Index.py
+++ 274-h-index/274. H-Index.py
@@ -1,14 +1,17 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort(reverse=True)
-        res = 0
+        #solution without sorting
+        temp = [0] * (len(citations) + 1)
 
         for i,n in enumerate(citations):
-            if res == len(citations):
-                return res
-            if n >= i+1:
-                res = max(res, i+1)
-        return res
-
-
+            if n > len(citations):
+                temp[-1] += 1
+            else:
+                temp[n] += 1
+        
+        res = 0
+        for i in range(len(citations), -1, -1):
+            res += temp[i]
+            if res >= i:
+                return i
         
