42a87dab50d388a431bf7f3fa28f4864cfc0f9f8
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-06-28 15:40:21 -0700

    Runtime: 273 ms (55.19%), Memory: 63.4 MB (73.31%) - Gitcode

---
+++ 380-insert-delete-getrandom-o1/380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,60 @@
+class RandomizedSet:
+    #array can do get random
+    #hashmap for complexity
+
+    def __init__(self):
+        self.arr = []
+        self.hmap = {}
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.hmap:
+            return False
+        
+
+        self.arr.append(val)
+        self.hmap[val] = len(self.arr) - 1
+
+
+
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        #removing the last element of an array is o(1)
+        if val not in self.hmap:
+            return False
+        
+        # print(self.arr, "asdf")
+        # print(val)
+        idx = self.hmap[val]
+        # print(idx)
+
+        #swap indexes on map
+        self.hmap[self.arr[idx]],  self.hmap[self.arr[-1]] =  self.hmap[self.arr[-1]],  self.hmap[self.arr[idx]]
+    
+        self.hmap.pop(val)
+
+
+
+        self.arr[idx], self.arr[-1] = self.arr[-1], self.arr[idx]
+        self.arr.pop() #value is removed
+
+        # print(self.arr, "asdf")
+
+        return True
+
+
+
+
+    def getRandom(self) -> int:
+        # print(self.arr)
+        return self.arr[randint(0, len(self.arr) - 1)]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
