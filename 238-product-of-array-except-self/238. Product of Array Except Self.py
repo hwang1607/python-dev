c2d44c995724d347304e2ed270741af23da24967
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-02 13:09:55 -0700

    Runtime: 282 ms (26.23%), Memory: 25.7 MB (51.74%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -1,14 +1,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1] * len(nums)
-        right = [1] * len(nums)
         res = [1] * len(nums)
 
         templeft = 1
         for i in range(1, len(nums)):
             templeft = nums[i-1] * templeft
             res[i] = templeft
-        print(res)
+        #print(res)
 
         tempright = 1
         for i in range(len(nums) - 2, -1, -1):
@@ -19,7 +17,6 @@ class Solution:
         # for i in range(len(nums)):
         #     res[i] = left[i] * right[i]
 
-        print(left, right)
         return res
             
         
