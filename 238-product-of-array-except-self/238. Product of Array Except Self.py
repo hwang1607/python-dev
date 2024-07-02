7dfcde50c9fa8e8cd09a703e22a4c2ca28645251
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-02 12:32:33 -0700

    Runtime: 242 ms (99.57%), Memory: 23.6 MB (99.97%) - Gitcode

--- 238-product-of-array-except-self/238. Product of Array Except Self.py
+++ 238-product-of-array-except-self/238. Product of Array Except Self.py
@@ -1,16 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        res = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
 
-
-        for i in range(len(nums)-1):
-            res[i+1] = nums[i] * res[i]
-    
-        prevvalue = 1
-        for i in range(len(nums) - 1, 0, -1):
-            prevvalue *= nums[i]
-            res[i-1] *= prevvalue
-
-
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
 
         return res
+
+
+            
