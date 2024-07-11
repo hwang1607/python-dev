c6992dcd80797bf2989920661bbd0fa8c3810969
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-10 19:52:09 -0700

    Runtime: 134 ms (20.48%), Memory: 19.3 MB (66.84%) - Gitcode

--- 135-candy/135. Candy.py
+++ 135-candy/135. Candy.py
@@ -1,18 +1,20 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         length = len(ratings)
-        arr = [1] * length
-
+        ltor = [1] * length
+        rtol = [1] * length
+        res = 0
+        #left to right and right to left
         for i in range(1, len(ratings)):
             if ratings[i] > ratings[i-1]:
-                arr[i] = arr[i-1] + 1
-
-        res = arr[-1] #this one isnt counted in loop
+                ltor[i] = ltor[i-1] + 1
+        
         for i in range(len(ratings) - 2, -1 , -1):
             if ratings[i] > ratings[i+1]:
-                arr[i] = max(arr[i], arr[i+1] + 1)
-            res += arr[i]
+                rtol[i] = rtol[i+1] + 1
         
+        for i in range(len(ratings)):
+            res += max(ltor[i],rtol[i])
 
         
         return res
