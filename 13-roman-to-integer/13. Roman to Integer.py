6c991acced910d9202bdf56098c1d402ba4fde1b
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-12 18:05:53 -0700

    Runtime: 42 ms (80.07%), Memory: 16.6 MB (36.61%) - Gitcode

--- 13-roman-to-integer/13. Roman to Integer.py
+++ 13-roman-to-integer/13. Roman to Integer.py
@@ -6,33 +6,13 @@ class Solution:
 
         i = 0
         while i < len(s):
-
-            if s[i] == "I" and i+1 < len(s) and (s[i+1] == "V" or s[i+1] == "X"):
-                if  s[i+1] == "V":
-                    res += 4
-                elif s[i+1] == "X":
-                    res += 9
-                i+=2
-                continue
             
-            if s[i] == "X" and i+1 < len(s) and (s[i+1] == "L" or s[i+1] == "C"):
-                if  s[i+1] == "L":
-                    res += 40
-                elif s[i+1] == "C":
-                    res += 90
+            if  i+1 < len(s) and nums[s[i+1]] > nums[s[i]]:
+                res += nums[s[i+1]] - nums[s[i]]
                 i+=2
-                continue
-
-            if s[i] == "C" and i+1 < len(s) and (s[i+1] == "D" or s[i+1] == "M"):
-                if  s[i+1] == "D":
-                    res += 400
-                elif s[i+1] == "M":
-                    res += 900
-                i+=2
-                continue
-
-            res += nums[s[i]]
-            i += 1
+            else:
+                res += nums[s[i]]
+                i += 1
         
         return res
         
