9732dccfba82f3921d34e3144ace317c3ee9f791
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-14 19:57:06 -0700

    Runtime: 87 ms (5.13%), Memory: 13.9 MB (100.00%) - Gitcode

--- 12-integer-to-roman/12. Integer to Roman.py
+++ 12-integer-to-roman/12. Integer to Roman.py
@@ -1,15 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-
-        nums = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
-        #{1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        
         res = ""
-
-        for n in nums:
-            res += (num // n) * nums[n]
-            num -= (num // n) * n
-            #number of times n goes into num
+        
+        for i in d:
+            res += (num // i) * d[i]
+            num %= i
+            
         
         return res
-
+         
         
