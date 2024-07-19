44a4191cd0546bace23ca847b707a1cb15a87729
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-18 22:41:23 -0700

    Runtime: 97 ms (11.25%), Memory: 16.6 MB (48.38%) - Gitcode

---
+++ 6-zigzag-conversion/6. Zigzag Conversion.py
@@ -0,0 +1,42 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        if numRows == 1:
+            return s
+        res = ""
+
+        space = numRows * 2 - 2
+        i = 0
+        while i < len(s):
+            res += s[i]
+            i += space
+
+
+        space = numRows * 2 - 2
+        #i = 1
+
+        minus = 2
+
+        for i in range(1, numRows-1):
+            j = i
+            print(j)
+
+            while j < len(s):
+                print(j,"d")
+                res += s[j]
+                print(res)
+                if j + space - minus < len(s):
+                    res += s[j+space- minus]
+
+                j += space
+            minus += 2
+
+        i = numRows - 1
+        while i < len(s):
+            res += s[i]
+            i += space
+
+
+        return res
+                
+        
