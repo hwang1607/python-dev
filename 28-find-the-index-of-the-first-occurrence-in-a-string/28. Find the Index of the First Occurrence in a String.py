a899e3f6013a8b69a299bc4e3a01bea7bd59d2c0
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-20 23:20:25 -0700

    Runtime: 29 ms (89.92%), Memory: 16.6 MB (20.55%) - Gitcode

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28. Find the Index of the First Occurrence in a String.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28. Find the Index of the First Occurrence in a String.py
@@ -3,14 +3,7 @@ class Solution:
         res = -1
 
         for i in range(len(haystack)):
-            temp = i
-            for j in range(len(needle)):
-
-                if temp >= len(haystack) or haystack[temp] != needle[j]:
-                    break
-                if j == len(needle) -1:
-                    return temp - len(needle) +1
-                temp += 1
-        
+            if haystack[i: i +len(needle)] == needle:
+                return i        
         return -1
         
