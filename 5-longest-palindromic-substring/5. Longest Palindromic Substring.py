917566f62503109ec2689674a420d0783936746f
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-25 22:53:43 -0700

    Runtime: 340 ms (59.18%), Memory: 16.6 MB (36.82%) - Gitcode

--- 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
+++ 5-longest-palindromic-substring/5. Longest Palindromic Substring.py
@@ -1,24 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        longest = 0
         res = ""
-        reslen = 0
 
-        def check(l,r):
-            nonlocal reslen
+        def check(l, r):
+            #check palindrome expanding from l and r
+
+            nonlocal longest
             nonlocal res
+
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > reslen:
-                    reslen = (r - l + 1)
+                if (r-l+1) > longest:
+                    longest = r-l+1
                     res = s[l:r+1]
                 l -= 1
                 r += 1
-
-
+            
+        
         for i in range(len(s)):
             check(i,i)
             check(i, i+1)
 
         return res
-
-
-            
