32d3bc843dadb60d0be5e302ea8113e1fe6ca2cd
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-26 19:40:03 -0700

    Runtime: 41 ms (16.95%), Memory: 16.5 MB (74.33%) - Gitcode

--- 1816-truncate-sentence/1816. Truncate Sentence.py
+++ 1816-truncate-sentence/1816. Truncate Sentence.py
@@ -1,6 +1,7 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
-        s = s.split(" ")
-        
+        s = s.split(maxsplit = k)
+        #default seperator is whitespace, maxsplit prevents splitting the whole thing
+
         return " ".join(s[:k])
         
