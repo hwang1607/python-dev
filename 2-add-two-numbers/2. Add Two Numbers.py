6829250d996d84c3f9dac70e92e56c09c92c4a73
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-28 11:40:41 -0700

    Runtime: 53 ms (68.32%), Memory: 16.6 MB (22.07%) - Gitcode

--- 2-add-two-numbers/2. Add Two Numbers.py
+++ 2-add-two-numbers/2. Add Two Numbers.py
@@ -3,31 +3,47 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #what we have here is a singly linked list with two non empty linked list representing two 
+        #non negative integers - stored in reverse order, add and return sum as linked list, also in reverse order it seems
 
-        dummy = head = ListNode()
+        #my first thought is to just convert the two linked lists into integers and then sum them then return the result as a linked list
 
+        #second thought is to do basic math and use a carry variable
+
+
+        dummy = ListNode()
+        head = dummy
         carry = 0
 
         while l1 or l2 or carry:
-            curr = carry
-
+            total = carry
             if l1:
-                curr += l1.val
+                total += l1.val
                 l1 = l1.next
             if l2:
-                curr += l2.val
+                total += l2.val
                 l2 = l2.next
-
-            carry = curr // 10
-            curr %= 10
             
-            dummy.next = ListNode(curr)
+            carry = total // 10
+            total %= 10
+            
+            dummy.next = ListNode(total)
             dummy = dummy.next
+
         
         return head.next
-            
+
+        
+
+
+
+
+
+
 
 
         
