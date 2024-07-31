16dd7635601634419ecbb50bb27359468e10b9e2
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-07-30 21:33:59 -0700

    Runtime: 982 ms (25.48%), Memory: 20.4 MB (87.36%) - Gitcode

--- 15-3sum/15. 3Sum.py
+++ 15-3sum/15. 3Sum.py
@@ -1,11 +1,12 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort()
+        nums = sorted(nums)
 
-        for i, n in enumerate(nums):
+        for i,n in enumerate(nums):
             if i > 0 and n == nums[i-1]:
                 continue
+            
             l = i+1
             r = len(nums) - 1
 
@@ -17,9 +18,9 @@ class Solution:
                     l += 1
                 else:
                     res.append([n, nums[l], nums[r]])
+                    r -= 1
+                    while nums[r] == nums[r+1] and l<r:
+                        r -= 1
 
-                    l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l += 1
         
         return res
