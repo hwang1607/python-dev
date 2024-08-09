a68410ccfd47bb61e5ce3dc7f814bc723aa545fa
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-08-09 15:07:39 -0700

    Runtime: 41 ms (73.20%), Memory: 16.4 MB (81.03%) - Gitcode

--- 1029-two-city-scheduling/1029. Two City Scheduling.py
+++ 1029-two-city-scheduling/1029. Two City Scheduling.py
@@ -1,20 +1,25 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         res = 0
-        arr = []
-        n = len(costs) / 2
-        for i,c in enumerate(costs):
-            arr.append([c[0] - c[1], i])
+        # arr = []
+        n = len(costs) // 2
+        # for i,c in enumerate(costs):
+        #     arr.append([c[0] - c[1], i])
 
-        arr.sort()
+        # arr.sort()
 
-        for x in arr:
-            if n > 0: #city a
-                res += costs[x[1]][0]
-                n -= 1
-            else: #city b
-                res += costs[x[1]][1]
+        costs.sort(key = lambda x : x[0] - x[1])
 
+        # for x in arr:
+        #     if n > 0: #city a
+        #         res += costs[x[1]][0]
+        #         n -= 1
+        #     else: #city b
+        #         res += costs[x[1]][1]
+
+        for i in range(n):
+            res += costs[i][0] + costs[i+n][1]
+        
 
 
         return res
