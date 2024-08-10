808b1ff19731486f00914577418ab94e2ab8bf34
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-08-10 16:42:27 -0700

    Runtime: 42 ms (15.04%), Memory: 16.4 MB (94.29%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -2,13 +2,13 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
 
-        for r in range(m-1):
+        for i in range(m-1):
             newrow = [1] * n
-
-            for c in range(n - 2, -1, -1):
-                newrow[c] = row[c] + newrow[c+1]
-
+            for j in range(n - 2, -1, -1):
+                newrow[j] = row[j] + newrow[j+1]
             row = newrow
-        
+
         return row[0]
+        
+
         
