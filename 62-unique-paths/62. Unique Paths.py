515d466c3450fa79afa83745f33b296d98ccff5e
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-08-11 21:28:10 -0700

    Runtime: 29 ms (90.85%), Memory: 16.6 MB (34.23%) - Gitcode

--- 62-unique-paths/62. Unique Paths.py
+++ 62-unique-paths/62. Unique Paths.py
@@ -4,15 +4,8 @@ class Solution:
         row[n-1] =1
 
         for i in reversed(range(m)):
-            #newrow = [1] * n
-            for j in range(n-2, -1, -1):
-                #newrow[j] = newrow[j+1] + row[j]
-            
+            for j in reversed(range(n-1)):
                 row[j] = row[j] + row[j+1]
             
-            #row = newrow
-
-
-
         return row[0]
         
