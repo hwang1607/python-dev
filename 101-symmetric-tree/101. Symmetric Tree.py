d0e05f276fd646f62fab188cb4eaba0f7901cf85
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-08-18 17:58:24 -0700

    Runtime: 30 ms (91.91%), Memory: 16.6 MB (53.79%) - Gitcode

---
+++ 101-symmetric-tree/101. Symmetric Tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        def dfs(p,q):
+            if not p and not q:
+                return True
+            
+            if not p or not q:
+                return False
+            
+            if p.val != q.val:
+                return False
+
+            #print(p.val, q.val)
+
+            if dfs(p.left, q.right) and dfs(p.right, q.left):
+                return True
+            
+            return False
+        
+        return dfs(root.left, root.right)        
