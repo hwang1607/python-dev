e110787ed7c92c690797edf6a5c6dd6ca9c50c5d
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-08-28 22:07:21 -0700

    Runtime: 1354 ms (5.05%), Memory: 13.8 MB (100.00%) - Gitcode

--- 509-fibonacci-number/509. Fibonacci Number.py
+++ 509-fibonacci-number/509. Fibonacci Number.py
@@ -4,13 +4,6 @@ class Solution:
             return 0
         if n == 1:
             return 1
-        
-        arr = [0] * (n+1)
-        
-        arr[1] = 1
-        for x in range(2, len(arr)):
-            arr[x] = arr[x-1] + arr[x-2]
-            
-        return(arr[n])
-            
+                
+        return self.fib(n - 1) + self.fib(n - 2)
         
