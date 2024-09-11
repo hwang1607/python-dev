fab61d306de899a40a1214a2118502cad8a35074
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-09-10 22:12:07 -0700

    Runtime: 118 ms (89.86%), Memory: 17.4 MB (71.79%) - Gitcode

---
+++ 931-minimum-falling-path-sum/931. Minimum Falling Path Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        dp = [0] * (len(matrix)) #row below
+
+        for r in matrix[::-1]:
+            temparr = [0] * (len(matrix)) 
+            for i, n in enumerate(r):
+                temparr[i] = n + min(dp[i-1] if i > 0 else float("inf"), dp[i], dp[i+1] if i+1 < len(matrix) else float("inf"))
+            dp = temparr
+
+        # print(dp)
+
+        return min(dp)
