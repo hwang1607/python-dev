762e365e15c3b3c5059cb7636c9c355dd97f96e5
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-11-01 11:08:14 -0700

    Runtime: 308 ms (60.36%), Memory: 18.1 MB (93.97%) - Gitcode

---
+++ 1957-delete-characters-to-make-fancy-string/1957. Delete Characters to Make Fancy String.py
@@ -0,0 +1,22 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+        prev1 = s[0]
+        prev2 = s[1]
+
+        res = s[0] + s[1]
+
+        for i in range(2, len(s)):
+            if prev1 == prev2 == s[i]:
+                pass
+            else:
+                res += s[i]
+            prev1 = prev2
+            prev2 = s[i]
+
+
+        return res
+
+            
+        
