a050d5cae8879a5bdcfc8fa705ce18b4e321386f
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2024-12-19 16:03:06 -0800

    Runtime: 66 ms (8.90%), Memory: 16.6 MB (90.59%) - Gitcode

--- 39-combination-sum/39. Combination Sum.py
+++ 39-combination-sum/39. Combination Sum.py
@@ -1,22 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        total = 0
-        arr = []
         res = []
-
-        def backtrack(i):
-            if sum(arr) == target:
-                res.append(arr[:])
-                return
-            if i == len(candidates) or sum(arr) > target:
-                return
-
-            arr.append(candidates[i])
-            backtrack(i)
-
-            arr.pop()
-            backtrack(i+1)
         
-        backtrack(0)
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            cur.append(candidates[i])
+            dfs(i, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+        dfs(0, [], 0)
         return res
-
+            
+            
