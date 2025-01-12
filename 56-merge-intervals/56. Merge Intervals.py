6ea1a0e8eda8e4f7763247e60a9f369043e89477
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-01-11 23:56:09 -0800

    Runtime: 7 ms (74.29%), Memory: 21 MB (38.57%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.py
+++ 56-merge-intervals/56. Merge Intervals.py
@@ -1,19 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort()
 
-        curr = intervals[0]
         res = []
 
-        for i in range(1, len(intervals)):
-            if intervals[i][0] > curr[1]: #not intersecting
-                res.append(curr)
-
-                curr = intervals[i]
-            else: #intersecting
-                curr[1] = max(curr[1], intervals[i][1])
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                #dont overlap
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
         
-        res.append(curr)
-    
         return res
         
