33d32956e19d396eac7cafb3b401107f00b80fd2
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-01-27 17:40:30 -0800

    Runtime: 126 ms (5.08%), Memory: 14.4 MB (100.00%) - Gitcode

--- 242-valid-anagram/242. Valid Anagram.py
+++ 242-valid-anagram/242. Valid Anagram.py
@@ -1,16 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmap = {}
-
+        map1 = {}
+        map2 = {}
+        
         if len(s) != len(t):
             return False
         
-        for i in range(len(s)):
-            hashmap[s[i]] = hashmap.get(s[i], 0) + 1
-            hashmap[t[i]] = hashmap.get(t[i], 0) - 1
+        for x in range(len(s)):
+            if s[x] not in map1:
+                map1[s[x]] = 0
+            if t[x] not in map2:
+                map2[t[x]] = 0
+            map1[s[x]] += 1
+            map2[t[x]] += 1
+            
+        if map1 == map2:
+            return True
+        else:
+            return False
+            
         
-        for h in hashmap.values():
-            if h:
-                return False
-
-        return True
+        
