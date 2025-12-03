71ddcfd91c21a2dc7b9143ffaf55fac575570e43
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-12-03 00:11:25 -0800

    Runtime: 31 ms (79.56%), Memory: 18.1 MB (19.89%) - Gitcode

--- 679-24-game/679. 24 Game.py
+++ 679-24-game/679. 24 Game.py
@@ -1,33 +1,37 @@
 class Solution:
-    def judgePoint24(self, cards: List[int]) -> bool:
+    # All possible operations we can perform on two numbers.
+    def generate_possible_results(self, a: float, b: float) -> List[float]:
+        res = [a + b, a - b, b - a, a * b]
+        if a:
+            res.append(b / a)
+        if b:
+            res.append(a / b)  
+        return res
+    
+    # Check if using current list we can react result 24.
+    def check_if_result_reached(self, cards: List[float]) -> bool:
+        # Base Case: We have only one number left, check if it is approximately 24.
+        if len(cards) == 1:
+            return abs(cards[0] - 24.0) <= 0.1
 
-        def solve(nums):
-            if len(nums) == 1:
-                return abs(nums[0] - 24) < 0.0001
-
-            for i in range(len(nums)):
-                for j in range(len(nums)):
-                    if i == j:
-                        continue
+        for i in range(len(cards)):
+            for j in range(i + 1, len(cards)):
+                # Create a new list with the remaining numbers and the new result.
+                new_list = [number for k, number in enumerate(cards) if (k != i and k != j)]
+                
+                # For any two numbers in our list, we perform every operation one by one.
+                for res in self.generate_possible_results(cards[i], cards[j]):
+                    # Add the new result to the list.
+                    new_list.append(res)
                     
-                    new_list = [nums[k] for k in range(len(nums)) if k!= i and k!=j]
-
-                    a,b = nums[i], nums[j]
-
-                    if solve(new_list + [a+b]):
+                    # Check if using this new list we can obtain the result 24.
+                    if self.check_if_result_reached(new_list):
                         return True
-                    if solve(new_list + [a-b]):
-                        return True
-                    if solve(new_list + [b-a]):
-                        return True
-                    if solve(new_list + [a*b]):
-                        return True
-
-
-                    if b != 0 and solve(new_list + [a/b]):
-                        return True
-                    if a!= 0 and solve(new_list + [b/a]):
-                        return True
-            return False
-        return solve([float(c) for c in cards])
-        
+                    
+                    # Backtrack: remove the result from the list.
+                    new_list.pop()
+                    
+        return False
+    
+    def judgePoint24(self, cards: List[int]) -> bool:
+        return self.check_if_result_reached(cards)
