c43c4a5eb7bc8e2aa9d5c32a1791e63d1f8735d9
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-12-07 22:24:39 -0800

    Runtime: 310 ms (10.11%), Memory: 20.1 MB (82.17%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,21 +1,27 @@
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
-                    count +=1
-        return count
-                    
-    
-    def dfs(self,grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>= len(grid[0]) or grid[i][j] != '1':
-            return
-        grid[i][j] = '2'
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
-        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        res = 0
+
+
+        def dfs(r,c):
+            grid[r][c] = "0" #marks it as seen
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+            for dr,dc in directions:
+                newrow = r + dr
+                newcol = c + dc
+
+                if newrow in range(ROWS) and newcol in range(COLS) and grid[newrow][newcol] == "1":
+                    dfs(newrow, newcol)
+
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r,c)
+                    res += 1
+        return res
