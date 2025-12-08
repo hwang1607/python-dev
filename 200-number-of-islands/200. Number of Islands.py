1a2d67e49c8403060713c55912bd19e1a2fbb0b6
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-12-07 22:12:33 -0800

    Runtime: 316 ms (8.87%), Memory: 23.9 MB (34.39%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,26 +1,37 @@
+
+# BFS Version From Video
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
-        ROWS = len(grid)
-        COLS = len(grid[0])
 
-        res = 0
-       
+        rows, cols = len(grid), len(grid[0])
+        visited=set()
+        islands=0
+
         def bfs(r,c):
-            grid[r][c] = 0
+             q = deque()
+             #visited.add((r,c))
+             grid[r][c] == 0
+             q.append((r,c))
+           
+             while q:
+                row,col = q.popleft()
+                directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+                for dr,dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                       
+                        q.append((r , c ))
+                        visited.add((r, c ))
 
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                if r+dr in range(ROWS) and c+dc in range (COLS) and grid[r+dr][c+dc] == "1":
-                    bfs(r + dr, c + dc)
-                
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1":
+        for r in range(rows):
+            for c in range(cols):
+               
+                if grid[r][c] == "1" and (r,c) not in visited:
                     bfs(r,c)
-                    res += 1
-        return res
-        
+                    islands +=1 
+
+        return islands
+
