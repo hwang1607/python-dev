e837b1c902620c0975a885c40b72620c7a180ceb
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-12-07 22:13:39 -0800

    Runtime: 960 ms (5.01%), Memory: 16.3 MB (100.00%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -1,37 +1,21 @@
 
-# BFS Version From Video
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
-        rows, cols = len(grid), len(grid[0])
-        visited=set()
-        islands=0
-
-        def bfs(r,c):
-             q = deque()
-             #visited.add((r,c))
-             grid[r][c] == 0
-             q.append((r,c))
-           
-             while q:
-                row,col = q.popleft()
-                directions= [[1,0],[-1,0],[0,1],[0,-1]]
-               
-                for dr,dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                       
-                        q.append((r , c ))
-                        visited.add((r, c ))
-
-        for r in range(rows):
-            for c in range(cols):
-               
-                if grid[r][c] == "1" and (r,c) not in visited:
-                    bfs(r,c)
-                    islands +=1 
-
-        return islands
-
+        count = 0 
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count +=1
+        return count
+                    
+    
+    def dfs(self,grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '2'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+        
