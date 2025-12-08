4d3cfa6d629fa65811b240e65e53d48a7b32d9c9
Henry Wang <54609782+hwang1607@users.noreply.github.com>
2025-12-07 22:27:55 -0800

    Runtime: 281 ms (19.63%), Memory: 25.5 MB (21.62%) - Gitcode

--- 200-number-of-islands/200. Number of Islands.py
+++ 200-number-of-islands/200. Number of Islands.py
@@ -4,24 +4,26 @@ class Solution:
         COLS = len(grid[0])
 
         res = 0
+        seen = set()
 
 
         def dfs(r,c):
-            grid[r][c] = "0" #marks it as seen
+            #grid[r][c] = "0" #marks it as seen
+            seen.add((r,c))
             directions = [[1,0],[-1,0],[0,1],[0,-1]]
 
             for dr,dc in directions:
                 newrow = r + dr
                 newcol = c + dc
 
-                if 0 <= newrow < ROWS and 0 <= newcol < COLS and grid[newrow][newcol] == "1":
+                if 0 <= newrow < ROWS and 0 <= newcol < COLS and grid[newrow][newcol] == "1" and (newrow,newcol) not in seen:
                     dfs(newrow, newcol)
 
 
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1":
+                if grid[r][c] == "1" and (r,c) not in seen:
                     dfs(r,c)
                     res += 1
         return res
